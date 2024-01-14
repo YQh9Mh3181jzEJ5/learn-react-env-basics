d6e04d9e0d4f98d4930ebffb82dff959c281c2f6
YQh9Mh3181jzEJ5 <mfghz487k44o6u6ekgub@gmail.com>
2024-01-14 19:53:15 +0900

    add: toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import ColorFullMessage from "./assets/components/ColorFullMessage";
 
 export const App = () => {
+  const [num, setNum] = useState(0);
+  const [isShowFace, setIsShowFace] = useState(true);
+
   const onclickButton = () => {
-    alert("");
+    setNum((prev) => prev + 1);
   };
+
+  const onClickToggle = () => {
+    setIsShowFace(!isShowFace);
+  };
+
   return (
     <div className="w-screen h-screen bg-gray-900 text-gray-200">
       <h1 className="text-2xl">こんにちは！</h1>
@@ -16,8 +25,16 @@ export const App = () => {
         onClick={onclickButton}
         className="p-2 bg-indigo-50 text-black rounded"
       >
-        ボタン
+        カウントアップ
       </button>
+      <p className="text-white">{num}</p>
+      <button
+        onClick={onClickToggle}
+        className="p-2 bg-indigo-50 text-black rounded"
+      >
+        ON/OFF
+      </button>
+      {isShowFace && <p>適当な文字を表示する</p>}
     </div>
   );
 };
